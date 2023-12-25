193fc02175aec2af732d3b83896cfa1cb53616b1
abdulsaabir <saabirhaaji10@gmail.com>
2023-12-25 21:29:11 +0300

    random day

--- 09.pack.py
+++ 09.pack.py
@@ -26,17 +26,20 @@ app.geometry('300x300')
 #     txt.set('')
 
 # txt = ctk.StringVar(value='')
-# label= ctk.CTkLabel(root,textvariable=txt)
+# label= ctk.CTkLabel(app,textvariable=txt)
 # label.pack()
-# text_entry = ctk.CTkEntry(root,textvariable=txt).pack()
-# button = ctk.CTkButton(root,text='click', command=printtext).pack()
+# text_entry = ctk.CTkEntry(app,textvariable=txt).pack()
+# button = ctk.CTkButton(app,text='click', command=printtext).pack()
+
+
+
 
 
 # ====================================== 3 ======================================
 # def checkbox_event():
 #     print("checkbox toggled, current value:", check_var.get())
 
-# check_var = ctk.StringVar(value="on")
+# check_var = ctk.StringVar()
 # checkbox = ctk.CTkCheckBox(app, text="CTkCheckBox", command=checkbox_event,
 #                                      variable=check_var)
 # checkbox.pack()
@@ -47,21 +50,51 @@ app.geometry('300x300')
 # checkbox.deselect()
 # checkbox.toggle()
 
+
+
+
 # ====================================== 3 ======================================
-def combobox_callback(choice):
-    print("combobox dropdown clicked:", choice)
+
+def radiobutton_event():
+    print("radiobutton toggled, current value:", radio_var.get())
+
+radio_var = ctk.StringVar()
+radiobutton_1 = ctk.CTkRadioButton(app, text="CTkRadioButton 1",
+                                             command=radiobutton_event, variable= radio_var, value='male')
+radiobutton_2 = ctk.CTkRadioButton(app, text="CTkRadioButton 2",
+                                             command=radiobutton_event, variable= radio_var, value='female')
+radiobutton_3 = ctk.CTkRadioButton(app, text="CTkRadioButton 3",
+                                             command=radiobutton_event, variable= radio_var, value='none')
+
+radiobutton_1.pack()
+radiobutton_2.pack()
+radiobutton_3.pack()
+
+ 
+# select ( selects the value no command action wil be triggered)
+# deselect  
+# invoke selected the value but also trigers the command
 
 
-# without variable
+
+
+
+
+# ====================================== 3 ======================================
+# def combobox_callback(choice):
+#     print("combobox dropdown clicked:", choice)
+
+
+# # without variable
 # combobox = ctk.CTkComboBox(app, values=["option 1", "option 2"],
-                                    #  command=combobox_callback)
+#                                      command=combobox_callback)
 
-# with variable 
-combobox_var = ctk.StringVar(value="option 2")
-combobox = ctk.CTkComboBox(app, values=["option 1", "option 2"],
-                                     command=combobox_callback, variable=combobox_var)
-combobox.pack()
-combobox.set("option 2")
+# # with variable 
+# combobox_var = ctk.StringVar(value="option 2")
+# combobox = ctk.CTkComboBox(app, values=["option 1", "option 2"],
+#                                      command=combobox_callback, variable=combobox_var)
+# combobox.pack()
+# combobox.set("option 2")
 
 
 
--- table.py
+++ table.py
@@ -1,26 +1,54 @@
 import customtkinter as ctk
 root = ctk.CTk()
-   
+import random
+root.geometry('300x300')
 
-# take the data
-lst = [(1,'Raj','Mumbai',19),
-       (2,'Aaryan','Pune',18),
-       (3,'Vaishnavi','Mumbai',20),
-       (4,'Rachna','Mumbai',21),
-       (5,'Shubham','Delhi',21)]
+# # take the data
+# lst = [(1,'Raj','Mumbai',19),
+#        (2,'Aaryan','Pune',18),
+#        (3,'Vaishnavi','Mumbai',20),
+#        (4,'Rachna','Mumbai',21),
+#        (5,'Shubham','Delhi',21)]
 
-# find total number of rows and
-# columns in list
-total_rows = len(lst)
-total_columns = len(lst[0])
+# # find total number of rows and
+# # columns in list
+# total_rows = len(lst)
+# total_columns = len(lst[0])
 
-for i in range(total_rows):
-    for j in range(total_columns):
-        e= ctk.CTkEntry(root, width=200, fg_color='blue',font=('Arial',16,'bold'))
-        e.grid(row=i, column=j)
-        e.insert("0", str(lst[i][j]))
+# for i in range(total_rows):
+#     for j in range(total_columns):
+#         e= ctk.CTkEntry(root, width=200, fg_color='blue',font=('Arial',16,'bold'))
+#         e.grid(row=i, column=j)
+#         e.insert("0", str(lst[i][j]))
+
+def hideAlldays():
+    if checkbox1.get():
+        for label in day_labels:
+            label.pack_forget()
+    else:  # If the checkbox is unchecked
+        for label in day_labels:
+            label.pack()
+
+def hideRandomDay():
+    if checkbox1.get():
+        randomDay= random.randint(0,6)
+        day_labels[randomDay].pack_forget()
+    else:
+        for label in day_labels:
+            label.pack()
+
+
+checkbox1 = ctk.CTkCheckBox(root, text='Hide All Days', command=hideAlldays)
+checkbox1.pack()
+checkbox1 = ctk.CTkCheckBox(root, text='Hide Random Days', command=hideRandomDay)
+checkbox1.pack()
  
-
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+day_labels = []
+for i in days_of_week:
+    label = ctk.CTkLabel(root,text=i)
+    label.pack()
+    day_labels.append(label)
 
   
 # create root window
